bb68baec5005219805d56c96e5feaeaeb138371c
waiwai2525 <waiwai.cps@gmail.com>
2023-07-18 15:27:52 +0900

    Appをfeature以下に移動

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { App } from "./App";
+import { App } from "./features/App/App";
 
 createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
