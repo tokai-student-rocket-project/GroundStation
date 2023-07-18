99646997aeb5b7acc869b4631d789052bdc28ce7
waiwai2525 <waiwai.cps@gmail.com>
2023-07-18 20:16:21 +0900

    🇮🇷

--- src/web/features/Main/children/TopPanel.tsx
+++ src/web/features/Main/children/TopPanel.tsx
@@ -6,19 +6,13 @@ export const TopPanel = () => {
   return (
     <div className="has-background-dark px-6 py-4">
       <div className="columns">
-        <div
-          className="column pb-2 m-2"
-          style={{ borderBottom: `solid 1px hsl(0, 0%, 96%)` }}
-        >
+        <div className="column pb-2 m-2">
           <TopLeftPanel />
         </div>
         <div className="column is-2 is-flex is-justify-content-center mb-3">
           <TopCenterPanel />
         </div>
-        <div
-          className="column pb-2 m-2"
-          style={{ borderBottom: `solid 1px hsl(0, 0%, 96%)` }}
-        >
+        <div className="column pb-2 m-2">
           <TopRightPanel />
         </div>
       </div>
