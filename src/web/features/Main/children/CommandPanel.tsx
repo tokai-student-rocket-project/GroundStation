e880017f99d789a1d05a7dc342fd80282a4ef844
waiwai2525 <waiwai.cps@gmail.com>
2023-07-20 20:52:00 +0900

    ✨ ショットカットキーでコマンドを実行できるように変更

--- src/web/features/Main/children/CommandPanel.tsx
+++ src/web/features/Main/children/CommandPanel.tsx
@@ -4,6 +4,15 @@ export const CommandPanel = () => {
       <h2 className="title is-4 has-text-light has-text-weight-light">
         COMMAND
       </h2>
+      <button
+        accessKey="f"
+        className="button is-primary is-fullwidth is-outlined"
+      >
+        <div>FLIGHT MODE ON</div>
+      </button>
+      <header className="header has-text-light is-flex is-justify-content-flex-end mx-4">
+        Ctrl + Opt + F
+      </header>
     </div>
   );
 };
