48acd0f91c70105c60b724e27112410d40fc717d
waiwai2525 <waiwai.cps@gmail.com>
2023-07-21 20:24:47 +0900

    serialportを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron-renderer";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,17 +10,25 @@ export default defineConfig({
     electron([
       {
         // Main-Process entry file of the Electron App.
-        entry: 'electron/main.ts',
+        entry: "electron/main.ts",
+        vite: {
+          build: {
+            rollupOptions: {
+              // Here are some C/C++ modules them can't be built properly
+              external: ["serialport"],
+            },
+          },
+        },
       },
       {
-        entry: 'electron/preload.ts',
+        entry: "electron/preload.ts",
         onstart(options) {
-          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
           // instead of restarting the entire Electron App.
-          options.reload()
+          options.reload();
         },
       },
     ]),
     renderer(),
   ],
-})
+});
