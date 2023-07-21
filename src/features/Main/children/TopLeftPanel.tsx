2c65fbad832ec64cf7fccb398b767e5046cebf32
waiwai2525 <waiwai.cps@gmail.com>
2023-07-22 06:46:10 +0900

    気温表示を追加

--- src/features/Main/children/TopLeftPanel.tsx
+++ src/features/Main/children/TopLeftPanel.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AirDataContext } from "../../App/App";
 
 const getNorm = (x?: number, y?: number, z?: number): number | undefined => {
--- src/features/Main/children/TopRightPanel.tsx
+++ src/features/Main/children/TopRightPanel.tsx
@@ -1,6 +1,15 @@
-import { useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
+
+import { AirDataContext } from "../../App/App";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTemperatureThreeQuarters,
+  faSnowflake,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const TopRightPanel = () => {
+  const { airData, setAirData, clearAirData } = useContext(AirDataContext);
   const [now, setNow] = useState<string>();
 
   useEffect(() => {
@@ -14,20 +23,60 @@ export const TopRightPanel = () => {
     <nav className="level is-justify-content-center">
       <div className="level-item has-text-centered">
         <div>
+          <p className="heading has-text-light">GNSS</p>
+          <p className="has-text-light">{"N  --°--'--.--\""}</p>
+          <p className="has-text-light">{"E ---°--'--.--\""}</p>
+        </div>
+      </div>
+      <div className="level-item has-text-centered">
+        <div>
+          <p className="heading has-text-light">Temperature</p>
+          <table width="100%">
+            <tbody>
+              <tr>
+                <td>
+                  <FontAwesomeIcon
+                    icon={faTemperatureThreeQuarters}
+                    size="lg"
+                    className="has-text-light mx-2"
+                  />
+                </td>
+                <td width="40m">
+                  <p className="has-text-light">
+                    {airData.outsideTemperature?.toFixed(1) ?? "--.-"}
+                  </p>
+                </td>
+                <td style={{ verticalAlign: "middle" }}>
+                  <p className="has-text-light mx-1">℃</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <FontAwesomeIcon
+                    icon={faSnowflake}
+                    size="lg"
+                    className="has-text-light mx-2"
+                  />
+                </td>
+                <td width="40m">
+                  <p className="has-text-light">{"--.-"}</p>
+                </td>
+                <td style={{ verticalAlign: "middle" }}>
+                  <p className="has-text-light mx-1">℃</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="level-item has-text-centered">
+        <div>
           <p className="heading has-text-light">Current Time</p>
           <p className="has-text-light" style={{ maxWidth: "200px" }}>
             {now}
           </p>
         </div>
       </div>
-      <div className="level-item has-text-centered">
-        <div>
-          <p className="heading has-text-light">GNSS</p>
-          <p className="has-text-light">{"N  --°--'--.--\""}</p>
-          <p className="has-text-light">{"E ---°--'--.--\""}</p>
-        </div>
-      </div>
-      <div className="level-item has-text-centered"></div>
     </nav>
   );
 };
