74d284586697bbca473de12ea051ba3f62d48ce3
waiwai2525 <waiwai.cps@gmail.com>
2023-07-22 02:27:57 +0900

    無効時の色を変更

--- src/utils/Color.ts
+++ src/utils/Color.ts
@@ -1,12 +1,12 @@
 export const getColorFromRssi = (rssi: number | undefined): string => {
-  if (!rssi) return "#B5B5B5";
+  if (!rssi) return "#7A7A7A";
   if (rssi < -120) return "#FF385F";
   if (rssi < -70) return "#FFDD56";
   return "#23D160";
 };
 
 export const getColorFromSnr = (snr: number | undefined): string => {
-  if (!snr) return "#B5B5B5";
+  if (!snr) return "#7A7A7A";
   if (snr < 1) return "#FF385F";
   if (snr < 4) return "#FFDD56";
   return "#23D160";
@@ -16,7 +16,7 @@ export const getColorFromDataRate = (
   dataRate: number | undefined,
   targetDataRate: number
 ): string => {
-  if (!dataRate) return "#B5B5B5";
+  if (!dataRate) return "#7A7A7A";
   if (dataRate < targetDataRate - targetDataRate / 0.5) return "#FF385F";
   if (dataRate < targetDataRate - targetDataRate / 0.25) return "#FFDD56";
   return "#23D160";
