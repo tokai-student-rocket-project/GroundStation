0d3b97ade988422f318edcc9c383cf7217a28689
waiwai2525 <waiwai.cps@gmail.com>
2023-07-25 21:50:15 +0900

    電力ソース表示を追加

--- src/features/Main/children/PowerSystemPanel.tsx
+++ src/features/Main/children/PowerSystemPanel.tsx
@@ -1,9 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PowerDataContext } from "../../App/App";
 
 export const PowerSystemPanel = () => {
   const { powerData, setPowerData, clearPowerData } =
     useContext(PowerDataContext);
+
+  const [isExternalSource, setIsExternalSource] = useState<boolean>(false);
+
+  const getSource = (batteryVoltage?: number, poolVoltage?: number): string => {
+    if (batteryVoltage == undefined || poolVoltage == undefined) return "----";
+
+    // LTC4416のデータシートから算出
+    const failVoltage = 15.8;
+    const restoreVoltage = 17.94;
+
+    if (isExternalSource) {
+      const canSwitchToBatterySource =
+        poolVoltage < batteryVoltage + batteryVoltage * 0.1 &&
+        poolVoltage < failVoltage;
+
+      if (canSwitchToBatterySource) {
+        setIsExternalSource(false);
+        return "BATT";
+      } else {
+        return "EXT";
+      }
+    } else {
+      const canSwitchToExternalSource =
+        poolVoltage > batteryVoltage - batteryVoltage * 0.1 &&
+        poolVoltage > restoreVoltage;
+
+      if (canSwitchToExternalSource) {
+        setIsExternalSource(true);
+        return "EXT";
+      } else {
+        return "BATT";
+      }
+    }
+  };
 
   return (
     <div
@@ -65,6 +99,25 @@ export const PowerSystemPanel = () => {
                   </td>
                   <td style={{ verticalAlign: "middle" }}>
                     <p className="has-text-light mx-1">V</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading has-text-light">Source</p>
+            <table width="100%">
+              <tbody>
+                <tr>
+                  <td width="64m">
+                    <p className="has-text-primary">
+                      {getSource(
+                        powerData.batteryVoltage,
+                        powerData.poolVoltage
+                      )}
+                    </p>
                   </td>
                 </tr>
               </tbody>
