1eae18b32fec788f7f9c2017cde2ae8ca6abb148
waiwai2525 <waiwai.cps@gmail.com>
2023-07-27 17:56:40 +0900

    姿勢角のモデルをH-57に変更

--- src/features/Main/children/OrientationPanel/children/OrientationModel.tsx
+++ src/features/Main/children/OrientationPanel/children/OrientationModel.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
+import { PerspectiveCamera, useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
-import { EulerOrder } from "three";
+import { EulerOrder, AxesHelper, GridHelper } from "three";
 
 type Props = {
   yaw?: number;
@@ -15,7 +15,7 @@ type RocketProps = {
 
 type GLTFResult = GLTF & {
   nodes: {
-    ["H-58_406K_500cc_(1)_v1"]: THREE.Mesh;
+    ["model"]: THREE.Mesh;
   };
   // eslint-disable-next-line @typescript-eslint/ban-types
   materials: {};
@@ -27,8 +27,8 @@ const Rocket = (props: RocketProps) => {
   return (
     <group {...props} dispose={null} scale={0.003}>
       <mesh
-        geometry={nodes["H-58_406K_500cc_(1)_v1"].geometry}
-        material={nodes["H-58_406K_500cc_(1)_v1"].material}
+        geometry={nodes["model"].geometry}
+        material={nodes["model"].material}
       />
     </group>
   );
@@ -50,7 +50,7 @@ const standardPitch = (pitch?: number): number => {
 
 const standardRoll = (roll?: number): number => {
   if (roll == undefined) return 0;
-  const deg = roll + 90;
+  const deg = roll;
   return deg * (Math.PI / 180);
 };
 
@@ -60,9 +60,32 @@ export const OrientationModel = ({ yaw, pitch, roll }: Props) => {
       className="has-background-black-ter"
       style={{ height: "200px", width: "200px" }}
     >
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+      <Canvas
+        camera={{
+          position: [4, 2, 4],
+          rotation: [
+            -15 * (Math.PI / 180),
+            45 * (Math.PI / 180),
+            0 * (Math.PI / 180),
+            "YXZ",
+          ],
+          isPerspectiveCamera: true,
+        }}
+      >
+        <directionalLight position={[1, 1, 1]} intensity={0.8} />
+        <ambientLight args={[0xffffff]} intensity={0.2} />
+
+        <primitive
+          object={new AxesHelper(10)}
+          rotation={[
+            standardYaw(yaw),
+            standardRoll(roll),
+            standardPitch(pitch),
+            "XZY",
+          ]}
+        />
+        <primitive object={new GridHelper()} />
+
         <Rocket
           rotation={[
             standardYaw(yaw),
