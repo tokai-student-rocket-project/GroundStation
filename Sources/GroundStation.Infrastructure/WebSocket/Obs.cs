b841937e51f8e4bbce737605c1a9d0f0ea2e0768
Yuki Iwai <waiwai.cps@gmail.com>
2024-03-03 00:23:53 +0900

    OBSへ提供するデータを変更

--- Sources/GroundStation.Infrastructure/WebSocket/Obs.cs
+++ Sources/GroundStation.Infrastructure/WebSocket/Obs.cs
@@ -39,6 +39,8 @@ public class Obs : IObsRepository
             return;
         }
         
+        _obs.SetInputSettings("gs_test", JObject.Parse("{ \"text\":\"OK\" }"));
+        
 
         if (flightData is not null)
         {
@@ -51,9 +53,9 @@ public class Obs : IObsRepository
             // _obs.SetInputSettings("gs_f_logger_usage", JObject.Parse("{ \"text\":\"" + flightData.LoggerDataPart.UsageString + "\" }"));
             // _obs.SetInputSettings("gs_f_logger_number", JObject.Parse("{ \"text\":\"" + flightData.LoggerDataPart.NumberString + "\" }"));
             //
-            // _obs.SetInputSettings("gs_flight_mode", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.ModeString + "\" }"));
-            // _obs.SetInputSettings("gs_flight_time", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.TimeString + "\" }"));
-            _obs.SetInputSettings("gs_flight_valveMode", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.ValveModeString + "\" }"));
+            _obs.SetInputSettings("gs_flight_mode", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.ModeString + "\" }"));
+            _obs.SetInputSettings("gs_flight_time", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.TimeString + "\" }"));
+            // _obs.SetInputSettings("gs_flight_valveMode", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.ValveModeString + "\" }"));
             // _obs.SetInputSettings("gs_flight_flightPinState", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.FlightPinStateString + "\" }"));
             // _obs.SetInputSettings("gs_flight_isFalling", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.IsFallingString + "\" }"));
             // _obs.SetInputSettings("gs_flight_sn3State", JObject.Parse("{ \"text\":\"" + flightData.FlightDataPart.SN3StateString + "\" }"));
@@ -70,8 +72,8 @@ public class Obs : IObsRepository
             // _obs.SetInputSettings("gs_valve_motorTemperature", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.MotorTemperatureString + "\" }"));
             // _obs.SetInputSettings("gs_valve_mcuTemperature", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.McuTemperatureString + "\" }"));
             // _obs.SetInputSettings("gs_valve_inputVoltage", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.InputVoltageString + "\" }"));
-            _obs.SetInputSettings("gs_valve_currentPosition", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.CurrentPositionString + "\" }"));
-            _obs.SetInputSettings("gs_valve_targetPosition", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.TargetPositionString + "\" }"));
+            // _obs.SetInputSettings("gs_valve_currentPosition", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.CurrentPositionString + "\" }"));
+            // _obs.SetInputSettings("gs_valve_targetPosition", JObject.Parse("{ \"text\":\"" + flightData.ValveDataPart.TargetPositionString + "\" }"));
             
         }
 
@@ -86,7 +88,7 @@ public class Obs : IObsRepository
             // _obs.SetInputSettings("gs_s_logger_usage", JObject.Parse("{ \"text\":\"" + sensingData.LoggerDataPart.UsageString + "\" }"));
             // _obs.SetInputSettings("gs_s_logger_number", JObject.Parse("{ \"text\":\"" + sensingData.LoggerDataPart.NumberString + "\" }"));
             //
-            // _obs.SetInputSettings("gs_dynamics_acceleration_norm", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.AccelerationNormString + "\" }"));
+            _obs.SetInputSettings("gs_dynamics_acceleration_norm", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.AccelerationNormString + "\" }"));
             // _obs.SetInputSettings("gs_dynamics_acceleration_x", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.AccelerationXString + "\" }"));
             // _obs.SetInputSettings("gs_dynamics_acceleration_y", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.AccelerationYString + "\" }"));
             // _obs.SetInputSettings("gs_dynamics_acceleration_z", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.AccelerationZString + "\" }"));
@@ -96,7 +98,7 @@ public class Obs : IObsRepository
             // _obs.SetInputSettings("gs_dynamics_force", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.ForceString + "\" }"));
             // _obs.SetInputSettings("gs_dynamics_jerk", JObject.Parse("{ \"text\":\"" + sensingData.DynamicsDataPart.JerkString + "\" }"));
             //
-            // _obs.SetInputSettings("gs_trajectory_altitude", JObject.Parse("{ \"text\":\"" + sensingData.TrajectoryDataPart.AltitudeString + "\" }"));
+            _obs.SetInputSettings("gs_trajectory_altitude", JObject.Parse("{ \"text\":\"" + sensingData.TrajectoryDataPart.AltitudeString + "\" }"));
             // _obs.SetInputSettings("gs_trajectory_verticalSpeed", JObject.Parse("{ \"text\":\"" + sensingData.TrajectoryDataPart.VerticalSpeedString + "\" }"));
             // _obs.SetInputSettings("gs_trajectory_apogee", JObject.Parse("{ \"text\":\"" + sensingData.TrajectoryDataPart.ApogeeString + "\" }"));
             // _obs.SetInputSettings("gs_trajectory_estimated", JObject.Parse("{ \"text\":\"" + sensingData.TrajectoryDataPart.EstimatedString + "\" }"));
@@ -112,7 +114,7 @@ public class Obs : IObsRepository
             // _obs.SetInputSettings("gs_electrical_power_ground", JObject.Parse("{ \"text\":\"" + sensingData.ElectricalDataPart.GroundPowerString + "\" }"));
             // _obs.SetInputSettings("gs_electrical_power_battery", JObject.Parse("{ \"text\":\"" + sensingData.ElectricalDataPart.BatteryPowerString + "\" }"));
             // _obs.SetInputSettings("gs_electrical_power_tie", JObject.Parse("{ \"text\":\"" + sensingData.ElectricalDataPart.TiePowerString + "\" }"));
-            // _obs.SetInputSettings("gs_electrical_power_bus", JObject.Parse("{ \"text\":\"" + sensingData.ElectricalDataPart.BusPowerString + "\" }"));
+            _obs.SetInputSettings("gs_electrical_power_bus", JObject.Parse("{ \"text\":\"" + sensingData.ElectricalDataPart.BusPowerString + "\" }"));
             //
             // _obs.SetInputSettings("gs_thermal_temperature_regulator1", JObject.Parse("{ \"text\":\"" + sensingData.ThermalDataPart.Regulator1TemperatureString + "\" }"));
             // _obs.SetInputSettings("gs_thermal_temperature_regulator2", JObject.Parse("{ \"text\":\"" + sensingData.ThermalDataPart.Regulator2TemperatureString + "\" }"));
